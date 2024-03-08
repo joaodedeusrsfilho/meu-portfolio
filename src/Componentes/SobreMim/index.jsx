5469e9c147875740cab1e4e452be98296ff73a26
João de Deus <joaodedeusrsfilho@gmail.com>
2024-03-08 16:44:01 -0300

    add file

--- src/Componentes/SobreMim/index.jsx
+++ src/Componentes/SobreMim/index.jsx
@@ -16,7 +16,7 @@ const SectionSobreMim = () => {
 
                     <img src="./imagens/perfilLinkedin.png" alt="foto_perfil_github" />
 
-                    <p className='paragrafo'>Sempre gostei da área de tecnologia por estar sempre em constante evolução e aperfeiçoamento, após ingressar na faculdade de ciências da computação e conhecer o mundo do desenvolvimento web percebi que essa é a área que desejo atuar, em busca disso estou diariamente me aperfeiçoando por meio das plataformas online: Alura, RocketSeat, DIO, fóruns, etc.
+                    <p className='paragrafo'>Sempre gostei da área de tecnologia por estar sempre em constante evolução e aperfeiçoamento, após ingressar na faculdade de ciências da computação e conhecer o mundo do desenvolvimento web percebi que essa é a área que desejo atuar.
                     </p>
                 </div>
 
