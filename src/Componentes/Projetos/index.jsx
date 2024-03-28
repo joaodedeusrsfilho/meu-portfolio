1bd8538ccf2aa959ec5684eaf36f3970778a02d0
João de Deus <joaodedeusrsfilho@gmail.com>
2024-03-28 13:09:40 -0300

    add file

--- src/Componentes/Projetos/index.jsx
+++ src/Componentes/Projetos/index.jsx
@@ -12,7 +12,7 @@ const Projetos = () => {
                 <div className="primeiraDiv">
                     
                     <div className="primeiraDiv1_1">
-                        <a href="https://github.com/joaodedeusrsfilho/projetoorgano" target='_blank'>
+                        <a href="https://hamburgueria-sable-nu.vercel.app/" target='_blank'>
 
                             <img src="./imagens/projetos/hamburgueria.png" alt="projetoSpotify"/>
                         </a>
@@ -21,7 +21,7 @@ const Projetos = () => {
                         </p>
                     </div>
                     <div className="primeiraDiv1_1">
-                        <a href="https://hamburgueria-sable-nu.vercel.app/" target='_blank'>
+                        <a href="https://github.com/joaodedeusrsfilho/projetoorgano" target='_blank'>
 
                             <img src="./imagens/projetos/organo.png" alt="projetoSpotify" />
                         </a>
