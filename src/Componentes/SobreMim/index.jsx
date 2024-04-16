b9ccc293d70e04416dc328f27a5024ad5894d265
João de Deus <joaodedeusrsfilho@gmail.com>
2024-04-16 11:52:28 -0300

    add file

--- src/Componentes/SobreMim/index.jsx
+++ src/Componentes/SobreMim/index.jsx
@@ -16,7 +16,7 @@ const SectionSobreMim = () => {
 
                     <img src="./imagens/formatura.jpg" alt="imagemFormado" />
 
-                    <p className='paragrafo'>Sou bacharel em ciências da computação, sempre gostei da área de tecnologia por estar sempre em constante evolução e aperfeiçoamento, após ingressar na faculdade de ciências da computação e conhecer o mundo do desenvolvimento web percebi que essa é a área que desejo atuar.
+                    <p className='paragrafo'>Sou bacharel em ciências da computação, sempre gostei da área de tecnologia por estar sempre em constante evolução e aperfeiçoamento, após ingressar e conhecer o mundo do desenvolvimento web percebi que essa é a área que desejo atuar.
                     </p>
                 </div>
 
