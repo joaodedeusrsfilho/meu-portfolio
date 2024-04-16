5c862f351a36136050b93e37f9dd22fa8f23b957
João de Deus <joaodedeusrsfilho@gmail.com>
2024-04-16 14:14:30 -0300

    add file

--- src/Componentes/Header/index.jsx
+++ src/Componentes/Header/index.jsx
@@ -27,7 +27,7 @@ const Header = () => {
                         <ul>
                             <li className='navItem'><a href="#">Inicio</a></li>
                             <li className='navItem'><a href="#sobre">Sobre Mim</a></li>
-                            <li className='navItem'><a href="#formacao">Formação</a></li>
+                            {/*<li className='navItem'><a href="#formacao">Formação</a></li>*/}
                             <li className='navItem'><a href="#hardSkill">Hard Skills</a></li>
                             <li className='navItem'><a href="#projetos">Projetos</a></li>
                         </ul>
