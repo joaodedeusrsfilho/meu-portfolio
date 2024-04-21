74fa9e66a6a8dbbcab06aca4611bdc5ba0661fd6
João de Deus <joaodedeusrsfilho@gmail.com>
2024-04-21 15:41:02 -0300

    add file

--- src/Componentes/Projetos/index.jsx
+++ src/Componentes/Projetos/index.jsx
@@ -10,25 +10,26 @@ const Projetos = () => {
                     <h1><IoConstruct /> Projetos <IoConstruct /></h1>
                 </div>
                 <div className="primeiraDiv">
-                    
+
                     <div className="primeiraDiv1_0">
                         <a href="https://hamburgueria-sable-nu.vercel.app/" target='_blank'>
 
-                            <img src="./imagens/projetos/hamburgueria.png" alt="projetoSpotify"/>
+                            <img src="./imagens/projetos/hamburgueria.png" alt="projetoSpotify" />
                         </a>
                         <p>
-                        Projeto Flash Burger com API WhatsApp, você é capaz de vender seus produtos seja o seu negócio uma hamburgueria, Pizzaria ou qualquer outro Delivery
+                            Projeto Flash Burger com API WhatsApp, você é capaz de vender seus produtos seja o seu negócio uma hamburgueria, Pizzaria ou qualquer outro Delivery
                         </p>
                     </div>
                     <div className="primeiraDiv1_1">
-                        <a href="https://github.com/joaodedeusrsfilho/projetoorgano" target='_blank'>
-
-                            <img src="./imagens/projetos/organo.png" alt="projetoSpotify" />
+                        <a href="https://instagram-clone-brown-eight.vercel.app/" target='_blank'>
+                            <img src="./imagens/projetos/cloneInstagram.png" alt="projetoSpotify" />
                         </a>
                         <p>
-                            Organizador de pessoas e times em um só lugar, utilizando React + Vite.
-                            Obs.: no campo imagem digite, por exemplo, a URL do GitHub seguida de (.png), dessa forma ira aparecer a imagem da foto do perfil do usuário do GitHub.
+                            Clone do Instagram utilizando flexbox e media query.
                         </p>
+
+
+
                     </div>
 
                     <div className="primeiraDiv1_2">
@@ -42,11 +43,13 @@ const Projetos = () => {
                 </div>
                 <div className="segundaDiv">
                     <div className="segundaDiv1_1">
-                        <a href="https://github.com/joaodedeusrsfilho/instagram-clone" target='_blank'>
-                            <img src="./imagens/projetos/cloneInstagram.png" alt="projetoSpotify" />
+                        <a href="https://github.com/joaodedeusrsfilho/projetoorgano" target='_blank'>
+
+                            <img src="./imagens/projetos/organo.png" alt="projetoSpotify" />
                         </a>
                         <p>
-                            Clone do Instagram utilizando flexbox e media query.
+                            Organizador de pessoas e times em um só lugar, utilizando React + Vite.
+                            Obs.: no campo imagem digite, por exemplo, a URL do GitHub seguida de (.png), dessa forma ira aparecer a imagem da foto do perfil do usuário do GitHub.
                         </p>
                     </div>
                     <div className="segundaDiv1_2">
@@ -58,7 +61,7 @@ const Projetos = () => {
                         </p>
                     </div>
                 </div>
-                
+
             </div>
         </section >
     )
